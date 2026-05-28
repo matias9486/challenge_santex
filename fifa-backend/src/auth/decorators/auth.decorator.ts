4af7a646490a7858ef058c5fbad426f89eb1b67d
matias9486 <matias9486mdp@gmail.com>
2026-05-27 22:42:01 -0300

    feat: agregado decorador compuesto que autentica y valida roles de una petición

---
+++ fifa-backend/src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,14 @@
+import { applyDecorators, UseGuards } from '@nestjs/common';
+import { ValidRoles } from '../enums/valid-roles.enum';
+import { RoleProtected } from './role-protected.decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { UserRoleGuard } from '../guards/user-role.guard';
+
+export function Auth(...roles: ValidRoles[]) {
+  console.log(roles);
+  return applyDecorators(
+    RoleProtected(...roles),
+
+    UseGuards(AuthGuard(), UserRoleGuard),
+  );
+}
