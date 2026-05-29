250d31a56486ab22ca8c58838f962a308a2b92d4
matias9486 <matias9486mdp@gmail.com>
2026-05-29 14:52:49 -0300

    feat: agregado decorador compuesto para agrupar decoradores de error de swagger.

---
+++ fifa-backend/src/common/decorators/error-response.decorators.ts
@@ -0,0 +1,23 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
+
+export function ErrorResponse() {
+  return applyDecorators(
+    ApiResponse({
+      status: 400,
+      description: 'Bad Request',
+    }),
+    ApiResponse({
+      status: 401,
+      description: 'Unauthorized',
+    }),
+    ApiResponse({
+      status: 403,
+      description: 'Forbidden',
+    }),
+    ApiResponse({
+      status: 500,
+      description: 'Internal Server Error.',
+    }),
+  );
+}
