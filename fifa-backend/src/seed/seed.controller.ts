06f9e1a547f23c6f38c102b256eb3119471e37bd
matias9486 <matias9486mdp@gmail.com>
2026-05-29 16:21:32 -0300

    update: controller documentado con swagger

--- fifa-backend/src/seed/seed.controller.ts
+++ fifa-backend/src/seed/seed.controller.ts
@@ -1,11 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ErrorResponse } from 'src/common/decorators/error-response.decorators';
 
+@ApiTags('Seed')
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Execute database seed',
+    description: 'This endpoint generates a default user and loads players from a CSV file into the database.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Seed executed successfully.',
+  })
+  @ErrorResponse()
   seed() {
     return this.seedService.executeSeed();
   }
