6a59cf22a2e3ccebd0c645b879d98428803ebef4
matias9486 <matias9486mdp@gmail.com>
2026-05-29 14:47:47 -0300

    udpate: configuración de swagger

--- fifa-backend/src/main.ts
+++ fifa-backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +19,27 @@ async function bootstrap() {
       },
     }),
   );
+
+  // Obtenemos el ConfigService para leer el .env
+  const configService = app.get(ConfigService);
+
+  // Configuración estructurada de CORS
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGINS'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Fifa Challenge API')
+    .setDescription('Fifa Challenge endpoints')
+    .setVersion('1.0')
+    //.addTag('players') agregaremos tags de otra manera
+    .addBearerAuth() //para autenticarnos en los endpoints que usen @ApiBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
