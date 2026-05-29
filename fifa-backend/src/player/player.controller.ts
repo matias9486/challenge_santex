88fdebb8d8fd4f06b4e2c2ada6bd562809c6465e
matias9486 <matias9486mdp@gmail.com>
2026-05-29 14:53:15 -0300

    update: controller documentado con swagger

--- fifa-backend/src/player/player.controller.ts
+++ fifa-backend/src/player/player.controller.ts
@@ -16,26 +16,82 @@ import { FilterPlayerDto } from './dto/filter-player.dto';
 import * as express from 'express';
 import { Auth, GetUser } from 'src/auth/decorators';
 import { User } from 'src/auth/entities/user.entity';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiProduces,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ResponsePlayerDto } from './dto/response-player.dto';
+import { ErrorResponse } from 'src/common/decorators/error-response.decorators';
+import { PaginatedPlayersDto } from './dto/paginated-player.dto';
 
+@ApiTags('Players')
+@ApiBearerAuth() // Indica que este endpoint o controller requiere token (ej. JWT)
 @Controller('players')
 @Auth() //cualquier acceso al controlador debe autenticarse
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
 
   @Post()
-  create(
-    @Body() createPlayerDto: CreatePlayerDto,
-    @GetUser() user: User
-  ) {
+  @ApiOperation({
+    summary: 'Create a new player',
+    description: 'Register a player and link it to the authenticated user.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Player created',
+    type: ResponsePlayerDto,
+  })
+  @ErrorResponse()
+  create(@Body() createPlayerDto: CreatePlayerDto, @GetUser() user: User) {
     return this.playerService.create(createPlayerDto, user);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Get the Player List',
+    description:
+      'It obtains player pagination to which search filters can be applied.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Paginated list of players retrieved successfully.',
+    type: PaginatedPlayersDto,
+  })
+  @ErrorResponse()
   findAll(@Query() filterPlayerDto: FilterPlayerDto) {
     return this.playerService.findAll(filterPlayerDto);
   }
 
   @Get('download')
+  @ApiOperation({
+    summary: 'Download players as CSV',
+    description:
+      'Generates and downloads a CSV file containing the players filtered by the provided criteria.',
+  })
+  @ApiProduces('text/csv') // Indica que este endpoint responde estrictamente con un CSV
+  @ApiOkResponse({
+    description: 'The CSV file has been successfully generated and downloaded.',
+    schema: {
+      type: 'string',
+      format: 'binary', // Esto le dice a Swagger que es un archivo descargable
+    },
+    headers: {
+      'Content-Type': {
+        schema: { type: 'string', example: 'text/csv; charset=utf-8' },
+      },
+      'Content-Disposition': {
+        schema: {
+          type: 'string',
+          example: 'attachment; filename="jugadores.csv"',
+        },
+      },
+    },
+  })
+  @ErrorResponse()
   async download(
     @Query() filterDto: FilterPlayerDto,
     @Res() res: express.Response,
@@ -59,11 +115,31 @@ export class PlayerController {
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get a Player by Id',
+    description: 'It obtains player by Id.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Player with id retrieved successfully.',
+    type: ResponsePlayerDto,
+  })
+  @ErrorResponse()
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.playerService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Update a Player by Id',
+    description: 'It updates a player by Id.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Player with id updated successfully.',
+    type: ResponsePlayerDto,
+  })
+  @ErrorResponse()
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updatePlayerDto: UpdatePlayerDto,
