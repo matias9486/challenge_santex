d6dcb5cbc1628143aa79a24f65daa6ce48d03336
matias9486 <matias9486mdp@gmail.com>
2026-05-31 23:25:09 -0300

    update: agregado httpClient y configuración para interceptors y uso de fetchApi

--- fifa-frontend/src/app/app.config.ts
+++ fifa-frontend/src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from '@core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient( // <-- Agregado para usar Http
+      withFetch(), // Activa el uso de la Fetch API
+      withInterceptors([ //agregado para usar interceptors propio        
+        authInterceptor
+      ]),
+    ),
+  ]
 };
