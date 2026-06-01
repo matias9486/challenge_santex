c66076d61e018230d0f7e8864862be008ff36754
matias9486 <matias9486mdp@gmail.com>
2026-05-31 23:47:25 -0300

    feat: agregado rutas para feature Auth

--- fifa-frontend/src/app/app.routes.ts
+++ fifa-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@core/guards/non-authenticated.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',        
+        loadChildren: () => import('@auth/auth.routes'), //usa el export default
+        canMatch: [
+            NotAuthenticatedGuard, //puede ingresar sino está autenticado            
+        ],
+    }
+    //ultimo iria el path vacio '' para la pagina principal
+];
