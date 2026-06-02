d4ac8b2725d4ec53b7910e1092212c9fecc49a77
matias9486 <matias9486mdp@gmail.com>
2026-06-02 10:11:06 -0300

    update: agregado rutas para Players con uso de AuthenticatedGuard

--- fifa-frontend/src/app/app.routes.ts
+++ fifa-frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AuthenticatedGuard } from '@core/guards/authenticated.guard';
 import { NotAuthenticatedGuard } from '@core/guards/non-authenticated.guard';
 
 export const routes: Routes = [
@@ -7,6 +8,13 @@ export const routes: Routes = [
         loadChildren: () => import('@auth/auth.routes'), //usa el export default
         canMatch: [
             NotAuthenticatedGuard, //puede ingresar sino está autenticado            
+        ],
+    },
+    {
+        path: 'players',        
+        loadChildren: () => import('@players/players.routes'), //usa el export default
+        canMatch: [
+            AuthenticatedGuard
         ],
     }
     //ultimo iria el path vacio '' para la pagina principal
