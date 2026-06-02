f055803c06998bfdaf3d8ad13ebb5e10ae021047
matias9486 <matias9486mdp@gmail.com>
2026-06-02 12:13:06 -0300

    udpate(shared): modificación del alert para que pueda cerrarse o no mediante parámetro.

--- fifa-frontend/src/app/shared/alert/alert.component.ts
+++ fifa-frontend/src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, model } from '@angular/core';
+import { Component, input, model } from '@angular/core';
 
 export type AlertType = 'success' | 'danger' | 'warning' | 'info';
 
@@ -18,8 +18,11 @@ export interface AlertData {
 export class AlertComponent {
  // Un 'model' en Angular es una Signal especial que permite comunicación de doble vía.
   // Al usar '.required', obligamos al padre a pasar el dato. 
-  // Al permitir '| null', dejamos que el estado inicial o final sea "vacío" (sin alerta).
+  // Al permitir '| null', dejamos que el estado inicial o final sea "vacío" (sin alerta).  
   message = model.required<AlertData | null>();
+
+  // Input para controlar si alert se puede cerrar. Por defecto es true, pero el padre puede pasarle false.
+  dismissible = input<boolean>(true);
 
   // Esto es un truco estándar para poder usar métodos globales como Array.isArray en el HTML
   protected readonly Array = Array;
